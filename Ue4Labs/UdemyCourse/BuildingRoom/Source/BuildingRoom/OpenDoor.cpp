231db424b0c0688789b92876e6e716d34d056cee
Nelson Rodrigues <nrodrigues@abyssal.eu>
2016-07-12 11:30:59 +0100

    UeLabs: SetUpLevel for Building scape

--- Ue4Labs/UdemyCourse/BuildingRoom/Source/BuildingRoom/OpenDoor.cpp
+++ Ue4Labs/UdemyCourse/BuildingRoom/Source/BuildingRoom/OpenDoor.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BuildingRoom.h"
-#include "PositionReport.h"
+#include "OpenDoor.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -17,21 +17,23 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
 	// ...
-   
-    FString ObjectName = GetOwner()->GetName();
-	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
-    UE_LOG(LogTemp, Warning, TEXT("Position report %s --> %s !"), *(ObjectName), *(ObjectPos));
+
+	AActor* Owner = GetOwner();
+
+	FRotator NewRotation = FRotator(0.0f, 25.0f, 0.0f);
 	
+	Owner->SetActorRotation(NewRotation);
+
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
