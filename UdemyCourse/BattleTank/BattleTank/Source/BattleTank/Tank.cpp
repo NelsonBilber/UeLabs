6c54eaf7e34fe42fa663a37c25fe6df384a491d0
Nelson Rodrigues <nrodrigues@abyssal.eu>
2016-10-04 19:02:55 +0100

    UeLabs: BlueprintReadOnly

--- UdemyCourse/BattleTank/BattleTank/Source/BattleTank/Tank.cpp
+++ UdemyCourse/BattleTank/BattleTank/Source/BattleTank/Tank.cpp
@@ -1,5 +1,6 @@
 #include "BattleTank.h"
 #include "TankAimingComponent.h"
+#include "TankMovementComponent.h"
 #include "Projectile.h"
 #include "TankBarrel.h"
 #include "Tank.h"
@@ -11,7 +12,8 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
     
-    TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
+    TankAimingComponent   = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
+	TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
 
 }
 
