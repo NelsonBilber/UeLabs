cf64d3a13fa350c67b5caa0bdc46e05d33f35450
Nelson Rodrigues <nbr@Nelsons-MBP.home>
2016-10-15 17:36:36 +0100

    UeLabs: Programatic sideways friction

--- UdemyCourse/BattleTank/BattleTank/Source/BattleTank/TankTrack.cpp
+++ UdemyCourse/BattleTank/BattleTank/Source/BattleTank/TankTrack.cpp
@@ -1,15 +1,28 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "BattleTank.h"
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
 
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+    auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+    
+    // Work-out the required acceleration this frame to correct
+    auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+    
+    // Calculate and apply sideways (F = m a)
+    auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+    auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Two tracks
+    TankRoot->AddForce(CorrectionForce);
+}
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	// TODO clamp actual throttle value so player can't over-drive
-	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
-	auto ForceLocation = GetComponentLocation();
-	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
-	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);	
+    auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+    auto ForceLocation = GetComponentLocation();
+    auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+    TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
--- UdemyCourse/BattleTank/BattleTank/Source/BattleTank/TankTrack.h
+++ UdemyCourse/BattleTank/BattleTank/Source/BattleTank/TankTrack.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "Components/StaticMeshComponent.h"
@@ -11,14 +9,20 @@
 UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+    
 public:
-	// Sets a throttle between -1 and +1
-	UFUNCTION(BlueprintCallable, Category = Input)
-	void SetThrottle(float Throttle);
-	
-	// Max force per track, in Newtons
-	UPROPERTY(EditDefaultsOnly)
-	float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank, and 1g accelleration
+    // Sets a throttle between -1 and +1
+    UFUNCTION(BlueprintCallable, Category = "Input")
+    void SetThrottle(float Throttle);
+    
+    // Max force per track, in Newtons
+    UPROPERTY(EditDefaultsOnly)
+    float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank, and 1g accelleration
+    
+private:
+    UTankTrack();
+    
+    virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+    
 };
