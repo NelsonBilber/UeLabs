dea9edc6a1859f50acfee5358dfb7f443dc47200
Nelson Rodrigues <nbr@Nelsons-MBP.home>
2016-10-16 19:16:18 +0100

    UeLabs: TakeDamage()

--- UdemyCourse/BattleTank/BattleTank/Source/BattleTank/Tank.cpp
+++ UdemyCourse/BattleTank/BattleTank/Source/BattleTank/Tank.cpp
@@ -1,20 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "BattleTank.h"
 #include "Tank.h"
 
 
-// Sets default values
 ATank::ATank()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+    PrimaryActorTick.bCanEverTick = false;
 }
 
 
-void ATank::BeginPlay()
+float ATank::TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser)
 {
-    Super::BeginPlay();
+    int32 DamagePoints = FPlatformMath::RoundToInt(DamageAmount);
+    int32 DamageToApply = FMath::Clamp(DamagePoints, 0, CurrentHealth);
+    
+    CurrentHealth -= DamageToApply;
+    if (CurrentHealth <= 0)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Tank died"))
+    }
+    return DamageToApply;
 }
-
-
--- UdemyCourse/BattleTank/BattleTank/Source/BattleTank/Tank.h
+++ UdemyCourse/BattleTank/BattleTank/Source/BattleTank/Tank.h
@@ -14,17 +14,21 @@ class BATTLETANK_API ATank : public APawn
 {
 	GENERATED_BODY()
 
-	
+public:
+    
+    ATank();
+    
+    virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 protected:
 
     UPROPERTY(BlueprintReadOnly)
 	UTankMovementComponent* TankMovementComponent = nullptr;
 
 private:
-	
-    // Sets default values for this pawn's properties
-	ATank();
+    UPROPERTY(EditDefaultsOnly, Category = "Setup")
+    int32 StartingHealth = 100;
     
-    virtual void BeginPlay() override;
+    UPROPERTY(VisibleAnywhere, Category = "Health")
+    int32 CurrentHealth = StartingHealth;
 		
 	};
